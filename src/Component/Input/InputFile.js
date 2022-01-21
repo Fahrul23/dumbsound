fbeb45c44f82ec0dcb440245d1e4726b9398f7f3
Fahrul ihsan <ihsanfahrul23@gmail.com>
2022-01-21 16:55:08 +0700

    payment success

--- src/Component/Input/InputFile.js
+++ src/Component/Input/InputFile.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import File from '../../assets/icon/file.png';
 import './input.scss'
-export default function InputFile({text}) {
+export default function InputFile({text, name, ...rest}) {
   return (
     <div class="file-input">
-        <input type="file" class="file" />
-        <label for="file">{text}</label>
+        <input type="file" id={name} class="file" {...rest}  />
+        <label for={name}>{text}</label>
         <div class="img-file">
             <img src={File} alt="file" />
         </div>
--- src/Pages/Payment/index.js
+++ src/Pages/Payment/index.js
@@ -1,41 +1,94 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Navbar from '../../Component/Navbar'
 import './payment.scss';
 import Input from '../../Component/Input';
 import Button from '../../Component/Button';
-import File from '../../assets/icon/file.png'
 import InputFile from '../../Component/Input/InputFile';
+import { API } from '../../Config/Api';
+import { useNavigate } from 'react-router-dom';
 
 export default function Payment() {
-    
+  const [preview, setPreview] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [userId, setUserId] = useState('')
+  const [attache, setAttache] = useState('')
+  let navigate = useNavigate();
+
+
+  const [form, setForm] = useState({
+    userId: "",
+    attache: ""
+  })
+
+  const handleChange = (e) => {
+    if (e.target.type === "file") {
+        let url = URL.createObjectURL(e.target.files[0]);
+        setPreview(url);
+        setAttache(e.target.files)
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    setLoading(true)
+        try {
+            e.preventDefault();
+            const config = {
+                Headers: {
+                  "Content-type": "multipart/form-data"
+                }
+            }
+            const formData = new FormData()
+            formData.set("userId",userId)
+            formData.set("attache", attache[0], attache[0].name)
+            
+            const response = await API.post('transaction',formData,config)
+            navigate('/')
+            console.log("response",response)
+            setLoading(false)
+        } catch (error) {
+            setLoading(false)
+            console.log(error.message)
+        }
+  }
     return (
         <div>
             <Navbar />
-            <div class="payment-wrapper">
-                <div class="payment">
+            <div className="payment-wrapper">
+                <div className="payment">
                     <h4>Premium</h4>
-                    <div class="desc">
+                    <div className="desc">
                         <p>Bayar sekarang dan nikmati streaming music yang kekinian dari </p>
-                        <p class="app-name">DUMB<span>SOUND</span></p>
+                        <p className="app-name">DUMB<span>SOUND</span></p>
                     </div>
-                    <div class="contact">
-                        <p class="app-name">DUMB<span>SOUND</span></p>
-                        <p class="number">: 0981312323</p>
+                    <div className="contact">
+                        <p className="app-name">DUMB<span>SOUND</span></p>
+                        <p className="number">: 0981312323</p>
                     </div>
-                    <form>
-                        <div class="form-payment">
-                            <div class="form-input">
-                                <Input type="text" placeholder="Input your account number" />
+                    <form onSubmit={handleSubmit}>
+                        <div className="form-payment">
+                            <div className="form-input">
+                                <Input 
+                                  type="text" 
+                                  name="userId" 
+                                  placeholder="Input your account number" 
+                                  onChange={(e) => setUserId(e.target.value)}
+                                />
                             </div>
-                            <InputFile text="Attache proof of transfer"/>
-                            {/* <div class="img-preview">
-                                <img src={File} alt="img-preview" />
-                            </div>         */}
-                            <div class="submit">
+                            <InputFile 
+                              name="attache" 
+                              text="Attache proof of transfer"
+                              onChange={handleChange}
+                              />
+                            {preview && (
+                              <div className="img-preview">
+                                <img src={preview} alt="img-preview" />
+                              </div>        
+                            )}
+                            <div className="submit">
                                 <Button 
-                                    type="submit" 
-                                    className="btn btn-full btn-orange" 
-                                    text="Pay"
+                                  type="submit" 
+                                  className="btn btn-full btn-orange" 
+                                  text="Pay"
                                 />
                             </div>
                         </div>
